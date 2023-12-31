a539c72fb3044f114f008b9bb5277f4f84841a53
Kiprotich-Amos <kiprotichamoh11@gmail.com>
2023-12-31 17:01:57 +0300

    second commit

--- DB/con/connectDb.php
+++ DB/con/connectDb.php
@@ -1,26 +1,36 @@
 <?php
-	/**
-	 * creating database class
-	 */
-	class DbConnection{
-		private $con;
+/**
+ * Creating a database class
+ */
+class DbConnection
+{
+    private $con;
 
-		public function __construct($serverName, $dbName, $userName, $password){
-			try {
-				$this->con= new PDO("mysql:host=$serverName;dbname=$dbName", $userName, $password);
-				$this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			} catch (Exception $e) {
-				echo "Connection failed: " . $e->getMessage();
-			}
-		}
-		public function getConnection() {
-        	return $this->con;
-    	}
-		public function isConnected(){
-			return $this->con !== null;
-		}
-	}
-	
-	// $dbConnection = new DbConnection("localhost", "testroot", "root", "");
+    public function __construct($serverName, $dbName, $userName, $password)
+    {
+        try {
+            $this->con = new PDO("mysql:host=$serverName;dbname=$dbName", $userName, $password);
+            $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
 
+    public function getConnection()
+    {
+        return $this->con;
+    }
+
+    public function isConnected()
+    {
+        return $this->con !== null;
+    }
+}
+
+// Example usage:
+// $dbConnection = new DbConnection("localhost", "testroot", "root", "");
+// if ($dbConnection->isConnected()) {
+//     $pdo = $dbConnection->getConnection();
+//     // Perform operations with $pdo
+// }
 ?>
