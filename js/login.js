a7b406de86386ec435a7c69e0e7f98ffe847bd77
Kiprotich-Amos <kiprotichamoh11@gmail.com>
2024-01-01 17:08:39 +0300

    third commit

--- js/login.js
+++ js/login.js
@@ -3,3 +3,9 @@ let auth = document.getElementsByTagName('author')
 for (var i = 0; i < auth.length; i++) {
   auth[i].style.display = "none";
 }
+
+
+document.getElementsById('test').innerHTML = "TESTING JS FUNCTIONALITY";
+
+
+
